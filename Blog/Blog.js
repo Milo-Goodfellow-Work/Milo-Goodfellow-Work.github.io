4c2ec498a7f535825cfdf13c8441120b8313ffed
Milo Goodfellow <milogoodfellowworkemail@gmail.com>
2019-08-05 22:39:33 -0400

    Blog addition
    The addition of a sudo blog

--- Blog/Blog.js
+++ Blog/Blog.js
@@ -1,15 +1,38 @@
-let LinkList, ToServe, CurrentTitle, CurrentBody;
+let BodyList, TitleList, ImageList, CurrentTitle, CurrentBody, CurrentImage, CompletedPage;
 
-LinkList = ['Post1.txt'];
+BodyList = [];
+TitleList = [];
+ImageList = [];
+CompletedPage = '';
 
-for(i=0; i<LinkList.length; i++){
-  $.get(LinkList[i], function(RawFileText){
-    console.log(RawFileText);
-    CurrentTitle = '';
-    CurrentBody = '';
-    ToServe += "";
+TitleList.push("Why do I have a blog now?");
+ImageList.push("Images/Post1.jpg");
+BodyList.push(`
 
-  })
 
+`);
+
+for(i=0; i<TitleList.length; ++i){
+  console.log(i);
+  CurrentTitle = TitleList[i];
+  CurrentBody = BodyList[i];
+  CurrentImage = ImageList[i];
+
+  CompletedPage += `
+          <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+              <img class="activator" src="`+CurrentImage+`"></img>
+            </div>
+            <div class="card-content">
+              <span class="card-title activator">`+CurrentTitle+`<i class="material-icons right">more_vert</i></span>
+            </div>
+            <div class="card-reveal">
+              <span class="card-title">`+CurrentTitle+`<i class="material-icons right">close</i></span>
+              <p>`+CurrentBody+`</p>
+            </div>
+          </div>
+  `;
 
 }
+
+$("body").find(".posts").html(CompletedPage);
