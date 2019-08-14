8ee47ea3ab83cb319305642e76d7619b5a384716
Milo Goodfellow <milogoodfellowworkemail@gmail.com>
2019-08-14 00:50:04 -0400

    Post9

--- Blog/Blog.js
+++ Blog/Blog.js
@@ -1,96 +1,54 @@
-let BodyList, TitleList, ImageList, DetailsList, DateList, CurrentTitle, CurrentBody, CurrentImage, CurrentDetails, CurrentDate, CompletedPage, WhileRun, IteratorRecord;
+//This constant contains what would normally be a magic string, the base url from which all posts are derived
+const allPostsUrl = "https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/Milo-Goodfellow-Work/Milo-Goodfellow-Work.github.io/master/Blog/Posts/Post";
 
-BodyList = [];
-TitleList = [];
-ImageList = [];
-DetailsList = [];
-DateList = [];
-CompletedPage = '';
-WhileRun = 0;
-
-IteratorRecord = 0;
-PostNum = 8;
-
-for(i=1;i<=PostNum;i++){
-
-	PageExample = fetch("https://cors-anywhere.herokuapp.com/https://raw.githubusercontent.com/Milo-Goodfellow-Work/Milo-Goodfellow-Work.github.io/master/Blog/Posts/Post"+String(i)).then(function(response) {
-	    return response.text();
-	}).then(function(response) {
-			let InitialList = textFileParser(response);
-			TitleList.push(InitialList[0]);
-			ImageList.push(InitialList[1]);
-			DateList.push(InitialList[2]);
-			DetailsList.push(InitialList[3]);
-			BodyList.push(InitialList[4]);
-
-	}).then(function(){
-		++IteratorRecord;
-		if(IteratorRecord == PostNum){
-			CompletedPage = beginIteration();
-			$("body").find(".interior-container").append(CompletedPage);
-
-		}
-	});
-
-}
-
-function textFileParser(file){
-	let file_list_store = [];
-	file_list_store = (String(file).replace("↵", "")).split(',');
-	return file_list_store;
-
-}
-
-/*
-TitleList.push("An absolutely horrendously unproductive day");
-ImageList.push("Images/Post7.jpg");
-DateList.push("8/11/2019");
-DetailsList.push(`
-<br/>
-Date: 8/11/2019
-<br/>
-Productive Time: 1h02m09s97
-<br/>
-Wasted Time: 2h18m56s94
-<br/>
-Unblocked sites: FALSE
-<br/>
-Joined debate discord: TRUE
-`);
-BodyList.push(`
-I wasted a large amount of time on discord.
-`);
-
-*/
-
-function beginIteration(){
-
-	for(i=TitleList.length-1; i >= 0 ; --i){
-	  CurrentTitle = TitleList[i];
-	  CurrentBody = BodyList[i];
-	  CurrentImage = ImageList[i];
-	  CurrentDetails = DetailsList[i];
-	  CurrentDate = DateList[i];
-
-	  CompletedPage += `
-	          <div class="card">
-	            <div class="card-image waves-effect waves-block waves-light">
-	              <img class="activator" src="`+CurrentImage+`"></img>
-	            </div>
-	            <div class="card-content">
-	              <span class="card-title activator">`+CurrentTitle+`<i class="material-icons right">more_vert</i></span>
-	              <p>Posted on: `+CurrentDate+`</p>
-	            </div>
-	            <div class="card-reveal">
-	              <span class="card-title">`+CurrentTitle+`<i class="material-icons right">close</i></span>
-	              <strong class="bold">`+CurrentDetails+`</strong>
-	              <p>`+CurrentBody+`</p>
-	            </div>
-	          </div>
-	  `;
+async function generateBlogData(postNum){
+	let completedPage = '';
+	for(let i = postNum; i>=1; i--){
+		const rawPage = await pageFetcher(i);
+		const parsedList = pageParser(rawPage);
+		completedPage += newPost(parsedList);
 
 	}
 
-	return CompletedPage;
+	return completedPage;
 
 }
+
+function pageParser(pageData){
+	const parsedData = pageData.replace('↵', '').split(',');
+	return parsedData;
+
+}
+
+async function pageFetcher(currentPostNum){
+	const fetchedReturn = await fetch(`${allPostsUrl + currentPostNum}`);
+	const fetchedPage = await fetchedReturn.text();
+	return fetchedPage;
+
+}
+
+function newPost(parsedList){
+	const newPost = `
+<div class="card">
+	<div class="card-image card-image-max-width waves-effect waves-block waves-light">
+		<img class="activator" src="${parsedList[1]}"></img>
+	</div>
+	<div class="card-content">
+		<span class="card-title activator">${parsedList[0]}<i class="material-icons right">more_vert</i></span>
+		<p>Posted on: ${parsedList[2]}</p>
+	</div>
+	<div class="card-reveal">
+		<span class="card-title">${parsedList[0]}<i class="material-icons right">close</i></span>
+		<strong class="bold">${parsedList[3]}</strong>
+		<p>${parsedList[4]}</p>
+	</div>
+</div>`;
+
+	return newPost;
+
+}
+
+generateBlogData(9).then(function(completedPage){
+	$(".interior-container").append(completedPage);
+
+})
